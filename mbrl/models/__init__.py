e97ae5779d9993eba9d2d67566a0a0aeb45ca773
fahmyadan <fahmy.adan14@imperial.ac.uk>
2024-03-13 10:46:41 +0000

    add gaussian cnn model

--- mbrl/models/__init__.py
+++ mbrl/models/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from .basic_ensemble import BasicEnsemble
+from .gaussian_cnn import GaussianCnn
 from .gaussian_mlp import GaussianMLP
 from .model import Ensemble, Model
 from .model_env import ModelEnv
