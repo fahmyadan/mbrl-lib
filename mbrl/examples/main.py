4f72c65932b5b6068a1373f38a096f732df2c183
fahmyadan <fahmy.adan14@imperial.ac.uk>
2024-05-01 12:33:14 +0100

    add all cfg to wandb tracking

--- mbrl/examples/main.py
+++ mbrl/examples/main.py
@@ -20,7 +20,7 @@ def run(cfg: omegaconf.DictConfig):
     np.random.seed(cfg.seed)
     torch.manual_seed(cfg.seed)
     if cfg.overrides.get('logging', None) and cfg.overrides.logging.get('wandb', None):
-        wandb_cfg = omegaconf.OmegaConf.to_container(cfg.overrides)
+        wandb_cfg = omegaconf.OmegaConf.to_container(cfg)
         wandb_run = wandb.init(project= cfg.overrides.logging.project_name, config=wandb_cfg, sync_tensorboard=True, monitor_gym=True)
         wanb_cbs = [WandbCallback('loss'), WandbCallback('reward')]
     else :
